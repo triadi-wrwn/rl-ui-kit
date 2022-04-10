6e78a54a4ab0fec1b31c926bbf24d44019a48d98
trdi.wrwn <rurouniadhi@gmail.com>
2022-04-10 22:35:01 +0700

    Deploy website - based on 5eb10b237b4195336c79701f02f4990f6b02c615

---
+++ assets/js/e16015ca.6a41ae5f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrl_ui_kit=self.webpackChunkrl_ui_kit||[]).push([[700],{5688:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","name":"hola","count":1,"permalink":"/blog/tags/hola"}')}}]);
