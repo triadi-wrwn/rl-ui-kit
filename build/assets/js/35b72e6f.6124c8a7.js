5eb10b237b4195336c79701f02f4990f6b02c615
trdi.wrwn <rurouniadhi@gmail.com>
2022-04-10 22:33:52 +0700

    test deploy

---
+++ build/assets/js/35b72e6f.6124c8a7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrl_ui_kit=self.webpackChunkrl_ui_kit||[]).push([[448],{6425:function(a){a.exports=JSON.parse('{"allTagsPath":"/rl-ui-kit/blog/tags","name":"facebook","count":1,"permalink":"/rl-ui-kit/blog/tags/facebook"}')}}]);
